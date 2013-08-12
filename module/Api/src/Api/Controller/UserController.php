b06dd564a442977def7cb44e0e8099f9e8d9983b
Ionut Caval <ionut.caval@softvision.ro>
2013-08-12 18:41:30 +0300

    fixed REST

--- module/Api/src/Api/Controller/UserController.php
+++ module/Api/src/Api/Controller/UserController.php
@@ -6,6 +6,7 @@ use Admin\Entity\User;
 use Zend\View\Model\JsonModel;
 use Application\Controller\EntityUsingRestController;
 use Doctrine\ORM\Query;
+use Application\Services\EntitySerializer;
 
 class UserController extends EntityUsingRestController
 {
@@ -13,10 +14,16 @@ class UserController extends EntityUsingRestController
     public function getList()
     {
         $em = $this->getEntityManager();
+        $users = $em->getRepository('Admin\Entity\User')->findBy(array(), array('email' => 'ASC'));
+        $user = $em->getRepository('Admin\Entity\User')->findOneBy(array('id' => 4));
 
-        $users = $em->newHydrator(Query::HYDRATE_ARRAY)->getRepository('Admin\Entity\User')->findBy(array(), array('email' => 'ASC'));
+        $serializer = new EntitySerializer($em); // Pass the EntityManager object
+        $users_array = $serializer->serialize($users);
 
-        return new JsonModel(array("users" => $users));
+        return new JsonModel(array(
+            "users" => $users_array,
+            "user" => $serializer->serialize($user),
+        ));
     }
 
     public function get($id)
